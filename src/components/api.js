db21b0a8293d16d8b41b4d8147c5029fa0b9d1bf
Godsmiracle001 <miraxzeegame@gmail.com>
2025-07-14 12:50:49 +0100

    api base changed

--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:5000/api',
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
+  //baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://quality-control-tklz.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
