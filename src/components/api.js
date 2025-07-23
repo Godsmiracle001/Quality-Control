fdedff4c9d5174ce8ad85d6eb7552c224209746b
Godsmiracle001 <miraxzeegame@gmail.com>
2025-07-23 11:50:12 +0100

    index and api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  //baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://quality-control-tklz.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
+    
+  
   },
 });
 
